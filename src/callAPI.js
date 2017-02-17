abc1168c49a571d4d9264cea906a082e677e37e2
Will Cook <hello@doubleucook.com>
2017-02-17 11:22:39 +0000

    added error response to api call

--- src/callAPI.js
+++ src/callAPI.js
@@ -3,9 +3,10 @@ var apiId = process.env.OPENWEATHERMAPAPI;
 
 module.exports = {
   getDegFromAPI_GET: function(location, callback) {
+    var errorMsg = '';
     var deg = 'an error occured';
     var speed = 'an error occured';
-    var locationInput = JSON.stringify(location + ',uk');
+    var locationInput = location + JSON.stringify(',uk');
     // console.log('location', location);
     var options = {
       url: 'http://api.openweathermap.org/data/2.5/find/?',
@@ -21,18 +22,24 @@ module.exports = {
     request(options, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         // console.log('body', body);
-        console.log('response', response);
-        console.log('body', body);
+        // console.log('response', response);
+        // console.log('body', body);
         deg = body.list[0].wind.deg;
         speed = body.list[0].wind.speed;
+        console.log('api call sucessful:', response.statusCode);
           // console.log(typeof(deg));
+        callback(deg, speed, errorMsg);
       } else if (error){
-        console.log('error', JSON.stringify(error));
-        deg = 'error: ' + JSON.stringify(error);
+        // console.log('An error happen error', JSON.stringify(error));
+        // deg = 'error: ' + JSON.stringify(error);
+        console.log('An error happened, try again later ', response.statusCode);
+        errorMsg = 'An error happened, try again later';
       } else {
-        deg = 'response: '+ JSON.stringify(response);
+        // deg = 'response: '+ JSON.stringify(response);
+        errorMsg = 'Got an error response, try again later ' + response.statusCode;
+        console.log('Error: ', response.statusCode);
+        callback(deg, speed, errorMsg);
       }
-      callback(deg, speed);
     });
   }
 };
--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ exports.handler = function(event, context, callback) {
 // var self = this;
 
 var handlers = {
-
   'LaunchRequest': function() {
     var say = 'Tell me the location you want wind info for';
     this.emit('sayIt', ':ask', say, 'Try again, I didn\'t get that');
@@ -26,79 +25,86 @@ var handlers = {
     // var yourLocation = "london";
     // yourLocation = JSON.stringify(yourLocation);
 
-    callAPI.getDegFromAPI_GET(yourLocation, ((deg, speed) =>{
-      var degResponse = deg;
-      var say = deg;
+    callAPI.getDegFromAPI_GET(yourLocation, ((deg, speed, errorMsg) => {
 
-      switch(true) {
-        case (degResponse >= 348.76 || degResponse < 11.25): // North
-          say = 'Northerly';
-          // console.log('North');
-          break;
-        case (degResponse >= 11.26 && degResponse < 33.75): // North North East
-          say = 'North North East';
-          // console.log('North North East');
-          break;
-        case (degResponse >= 33.76 && degResponse < 56.25): // North East
-          say = 'Nor Easterly';
-          // console.log('North East');
-          break;
-        case (degResponse >= 56.26 && degResponse < 78.75): // East North East
-          say = 'East North East';
-          // console.log('East North East');
-          break;
-        case (degResponse >= 78.26 && degResponse < 101.25): // East
-          say = 'Easterly';
-          // console.log('East');
-          break;
-        case (degResponse >= 101.26 && degResponse < 123.75): // East South East
-          say = 'East South East';
-          // console.log('East South East');
-          break;
-        case (degResponse >= 123.76 && degResponse < 146.25): // South East
-          say = 'South Easterly';
-          // console.log('South East');
-          break;
-        case (degResponse >= 146.26 && degResponse < 168.75): // South South East
-          say = 'South South East';
-          // console.log('South South East');
-          break;
-        case (degResponse >= 168.76 && degResponse < 191.25): // South
-          say = 'Southerly';
-          // console.log('South');
-          break;
-        case (degResponse >= 191.26 && degResponse < 213.75): // South South West
-          say = 'South South West';
-          // console.log('South South West');
-          break;
-        case (degResponse >= 213.76 && degResponse < 236.25): // South West
-          say = 'South Western';
-          // console.log('South West');
-          break;
-        case (degResponse >= 236.26 && degResponse < 258.75): // West South West
-          say = 'West South West';
-          // console.log('West South West');
-          break;
-        case (degResponse >= 258.76 && degResponse < 281.25): // West
-          say = 'Westerly';
-          // console.log('West');
-          break;
-        case (degResponse >= 281.26 && degResponse < 303.75): // West North West
-          say = 'West North West';
-          // console.log('West North West');
-          break;
-        case (degResponse >= 303.76 && degResponse < 326.25): // North West
-          say = 'Norwesterly';
-          // console.log('North West');
-          break;
-        case (degResponse >= 326.26 && degResponse < 348.75): // West North West
-          say = 'West North West';
-          // console.log('West North West');
-          break;
+      if (!errorMsg) {
+        // API call was sucessful
+        var degResponse = deg;
+        var say = deg;
+
+        switch(true) {
+          case (degResponse >= 348.76 || degResponse < 11.25): // North
+            say = 'Northerly';
+            // console.log('North');
+            break;
+          case (degResponse >= 11.26 && degResponse < 33.75): // North North East
+            say = 'North North East';
+            // console.log('North North East');
+            break;
+          case (degResponse >= 33.76 && degResponse < 56.25): // North East
+            say = 'Nor Easterly';
+            // console.log('North East');
+            break;
+          case (degResponse >= 56.26 && degResponse < 78.75): // East North East
+            say = 'East North East';
+            // console.log('East North East');
+            break;
+          case (degResponse >= 78.26 && degResponse < 101.25): // East
+            say = 'Easterly';
+            // console.log('East');
+            break;
+          case (degResponse >= 101.26 && degResponse < 123.75): // East South East
+            say = 'East South East';
+            // console.log('East South East');
+            break;
+          case (degResponse >= 123.76 && degResponse < 146.25): // South East
+            say = 'South Easterly';
+            // console.log('South East');
+            break;
+          case (degResponse >= 146.26 && degResponse < 168.75): // South South East
+            say = 'South South East';
+            // console.log('South South East');
+            break;
+          case (degResponse >= 168.76 && degResponse < 191.25): // South
+            say = 'Southerly';
+            // console.log('South');
+            break;
+          case (degResponse >= 191.26 && degResponse < 213.75): // South South West
+            say = 'South South West';
+            // console.log('South South West');
+            break;
+          case (degResponse >= 213.76 && degResponse < 236.25): // South West
+            say = 'South Western';
+            // console.log('South West');
+            break;
+          case (degResponse >= 236.26 && degResponse < 258.75): // West South West
+            say = 'West South West';
+            // console.log('West South West');
+            break;
+          case (degResponse >= 258.76 && degResponse < 281.25): // West
+            say = 'Westerly';
+            // console.log('West');
+            break;
+          case (degResponse >= 281.26 && degResponse < 303.75): // West North West
+            say = 'West North West';
+            // console.log('West North West');
+            break;
+          case (degResponse >= 303.76 && degResponse < 326.25): // North West
+            say = 'Norwesterly';
+            // console.log('North West');
+            break;
+          case (degResponse >= 326.26 && degResponse < 348.75): // West North West
+            say = 'West North West';
+            // console.log('West North West');
+            break;
+        }
+        var speedinMPH = (parseFloat(speed) * 2.236936).toFixed(2);
+
+        this.emit('sayIt', ':tell', 'It is blowing ' + speedinMPH + ' miles per hour, in a ' + say + ' direction in ' + yourLocation);
+      } else {
+        // An error happened with the api call
+        this.emit('sayIt', ':tell', errorMsg);
       }
-      var speedinMPH = parseFloat((parseFloat(speed)) * 2.236936).toFixed(2);
-
-      this.emit('sayIt', ':tell', 'It is blowing ' + speedinMPH + ' miles per hour, in a ' + say + ' direction in ' + yourLocation);
     }));
   }
 };
